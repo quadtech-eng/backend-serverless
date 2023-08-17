da7dafd6baf0545521d35b6c401ae49ac0bd50c4
Wesley Dias <wesley.dossantosdias.ext@bayer.com>
2023-08-17 15:39:51 +0930

    update region on test

--- src/utils/secretsmanager.ts
+++ src/utils/secretsmanager.ts
@@ -6,6 +6,7 @@ const isLocal = process.env.IS_OFFLINE;
 if (isLocal) {
   const host = process.env.LOCALSTACK_HOST || "localhost";
   smClient = new SecretsManagerClient({
+    region: "us-east-1",
     credentials: {
       accessKeyId: "test",
       secretAccessKey: "test",
--- src/utils/ssm.ts
+++ src/utils/ssm.ts
@@ -7,6 +7,7 @@ const isLocal = process.env.IS_OFFLINE;
 if (isLocal) {
   const host = process.env.LOCALSTACK_HOST || "localhost";
   ssmClient = new SSMClient({
+    region: "us-east-1",
     credentials: {
       accessKeyId: "test",
       secretAccessKey: "test",
